1f8cd446b58febab7e7b1f5b4135afb8ce5d18db
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-13 16:27:44 -0800

    First attempt to using youtube api search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+
+YTSearch({key: process.env.API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
