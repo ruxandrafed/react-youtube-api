8cc4b25b64d3aa519f0d3b8a0230e15a5c53afc4
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-14 21:03:17 -0800

    Start off app with some state (call youtube api)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-YTSearch({key: process.env.API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    YTSearch({key: process.env.API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
