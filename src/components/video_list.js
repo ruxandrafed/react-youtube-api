2e869c376c69786f553801f62cccfe773fe99c45
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-14 21:35:36 -0800

    Update video list item to show title and thumbnail

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video}/>
+    return <VideoListItem video={video} key={video.etag}/>
   });
   return (
     <ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const title = video.snippet.title;
   return (
-    <li>Video {props.video.snippet.title}</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {title}
+          </div>
+        </div>
+      </div>
+    </li>
   );
 };
 
